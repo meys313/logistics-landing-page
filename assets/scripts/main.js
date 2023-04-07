fbc185b19ca239cb43842fb22b0835cce02d1b4f
meys313 <doesis04@gmail.com>
2023-04-07 23:41:28 +0300

    класс active для навигации, переход к разделам через навигацию

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,64 @@
 "use strict"
 
-// DOM элементы
-const blockIntro = $('#intro');
-const blockHeader = $('header');
+$(function (){
+
+    //
+    // DOM элементы
+    const blockIntro = $('#intro');
+    const blockHeader = $('header');
 
 
-const blockIntroHeight = blockIntro.innerHeight();
+    const blockIntroHeight = blockIntro.innerHeight();
 
-$(window).on("scroll", function (event){
-    let windowTopPosition = $(this).scrollTop();
-    if( windowTopPosition >= blockIntroHeight){
-        blockHeader.addClass("header--scroll")
-    } else{
-        blockHeader.removeClass("header--scroll")
+    $(window).on("scroll", function (event){
+       headerScroll();
+    });
+
+
+    const headerScroll = () => {
+        let windowTopPosition = $(this).scrollTop();
+        if( windowTopPosition >= blockIntroHeight){
+            blockHeader.addClass("header--scroll")
+        } else{
+            blockHeader.removeClass("header--scroll")
+        }
+
+
     }
+
+
+
+    $("[data-scroll]").on("click", function (event){
+        event.preventDefault();
+        let element = $(this).data("scroll");
+        let elementPosition = $(element).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elementPosition
+        }, 500)
+    })
+
+
+    let windowHeight = $(window).height();
+
+    $(window).on("scroll", function (){
+        let windowTopPosition = $(this).scrollTop();
+        $("[data-scrollspy]").each(function (){
+           let section = $(this).data('scrollspy')
+           let sectionTopPosition = $(this).offset().top - (windowHeight * 0.3);
+
+            if(windowTopPosition >= sectionTopPosition){
+                $(".nav [data-scroll]").removeClass('active')
+                $(`.nav [data-scroll="#${section}"]`).addClass('active')
+           }
+
+            if(windowTopPosition === 0){
+                $(".nav [data-scroll]").removeClass('active')
+            }
+        })
+    });
 });
+
+
+
 
